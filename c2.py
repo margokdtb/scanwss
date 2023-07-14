e08e254cce0df914dfe1475c408a24648665e371
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-14 10:47:12 +0700

    Add files via upload

--- c2.py
+++ c2.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import time
 
 def tcping(host, port):
@@ -13,21 +14,46 @@ def tcping(host, port):
         return round(elapsed_time, 2)
     except socket.error:
         return None
+        
+def ping_host(host):
+    response_time_80 = tcping(host, 80)
+    response_time_443 = tcping(host, 443)
+    if response_time_80 is not None:
+        print(f"{host} - 80 - {response_time_80*100:.2f}")
+    else:
+        print(f"Host: {host} - 80 - Tidak dapat dijangkau")
+    if response_time_443 is not None:
+        print(f"{host} - 443 - {response_time_443*100:.2f}")
+    else:
+        print(f"Host: {host} - 443 - Tidak dapat dijangkau")
+    result_list.append((host, 80, response_time_80))
+    result_list.append((host, 443, response_time_443))
 
 host_list = []
-
 # Baca sumber dari file xyz3.txt
 with open("hasil2.txt", "r") as file:
     for line in file:
         host_list.append(line.strip())
 
-port = 80
+# Lakukan ping secara multithread
+threads = []
+result_list = []
 
-# Lakukan ping ke setiap host dan simpan hasilnya di file xyz6.txt
+for host in host_list:
+    t = threading.Thread(target=ping_host, args=(host,))
+    t.start()
+    threads.append(t)
+
+# Tunggu semua thread selesai
+for t in threads:
+    t.join()
+    
+
+# Simpan hasil ping ke dalam file pjng-hasil.txt
 with open("ping-hasil.txt", "w") as file:
-    for host in host_list:
-        response_time = tcping(host, port)
+    for result in result_list:
+        host, port, response_time = result
         if response_time is not None:
-            file.write(f"{host} -  {response_time:.3f}\n")
+            file.write(f"{host} - {response_time*100:.2f} - {port} \n")
         else:
-            file.write(f"Host: {host} - Tidak dapat dijangkau\n")
+            file.write(f"Host: {host} - {port} - Tidak dapat dijangkau\n")
